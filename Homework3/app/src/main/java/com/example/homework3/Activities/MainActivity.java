3744d85da1f1643c69501dba434e012111561345
mikayla <z5209016@ad.unsw.edu.au>
2019-11-15 15:11:12 +1100

    Create MainActivity.java

---
+++ Homework3/app/src/main/java/com/example/homework3/Activities/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.homework3.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.homework3.Fragments.FavouritesFragment;
+import com.example.homework3.Fragments.SearchCatsFragment;
+import com.example.homework3.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = new SearchCatsFragment();
+        swapFragment(fragment);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.search_nav) {
+                    Fragment fragment = new SearchCatsFragment();
+                    swapFragment(fragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.favourites_nav) {
+                    Fragment fragment = new FavouritesFragment();
+
+                    Bundle bundle = new Bundle();
+                    fragment.setArguments(bundle);
+
+                    swapFragment(fragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void swapFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_slot, fragment);
+        fragmentTransaction.commit();
+    }
+}
