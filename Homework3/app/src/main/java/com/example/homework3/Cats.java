f86206558cfc180beb9687c8ad07ce12bd0e86c2
mikayla <z5209016@ad.unsw.edu.au>
2019-11-15 15:11:17 +1100

    Create Cats.java

---
+++ Homework3/app/src/main/java/com/example/homework3/Cats.java
@@ -0,0 +1,48 @@
+package com.example.homework3;
+
+import java.util.ArrayList;
+
+public class Cats {
+
+    //cat attributes
+    private String id;
+    private String name;
+    private String description;
+    private String weight_imperial;
+    private String temperament;
+    private String origin;
+    private String life_span;
+    private String wikipedia_url;
+    private int dog_friendly;
+
+
+//    public String getId() {
+//        return id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public String getWeight_imperial() {
+        return weight_imperial;
+    }
+    public String getTemperament() {
+        return temperament;
+    }
+    public String getOrigin() {
+        return origin;
+    }
+    public String getLife_span() {
+        return life_span;
+    }
+    public String getWikipedia_url() {
+        return wikipedia_url;
+    }
+    public int getDog_friendly() {
+        return dog_friendly;
+    }
+}
